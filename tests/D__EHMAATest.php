719a9eb84f49aa45d8bc645689a86591da610768
rgg7888 <rgg7888@outlook.com>
2021-05-05 16:32:43 -0600

    creando un id

--- tests/D__EHMAATest.php
+++ tests/D__EHMAATest.php
@@ -1,18 +1,39 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-use D__EHMAA;
+# COLOR CODE
+//? keyword
+//* class
+//^ method
+#the variable $objetoD__EHMAA is an object
 
+use PHPUnit\Framework\TestCase; //phpUnit class
+
+use D__EHMAA; //my class
+
+# my class //? extends(hereda de) 
+#phpUnit class
 class D__EHMAATest extends TestCase
+
+//& bloque de codigo (code Block)
 {
+
+    //~ method (function) inside a class
     public function testInstancia(): void
     {
-        
+
+        //TODO objeto de la Instanciacion de la clase D__EHMAA
+        //TODO INSTANCE
         $objetoD__EHMAA = D__EHMAA::instancia();
 
+        //! confirmation of $objetoD__EHMAA , $objetoD__EHMAA is a D__EHMAA class
         $this->assertInstanceOf(D__EHMAA::class, $objetoD__EHMAA );
 
+        //utilizando el metodo ok() de la clase D__EHMAA desde el objeto $objetoD__EHMAA
+        //& this confirm that the ok() method return the string "OK !"
         $this->assertEquals('OK !',$objetoD__EHMAA->ok());
 
+        $this->assertEquals(' id="myId"',$objetoD__EHMAA->atributos(['id' => 'myId']));
+
     }
+
 }
