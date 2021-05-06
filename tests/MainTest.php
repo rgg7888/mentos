e15fc5a81efc0eecd0082934800d1d56baaceb9a
rgg7888 <rgg7888@outlook.com>
2021-05-05 18:21:48 -0600

    all the MainClassTest

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -19,11 +19,38 @@ class MainTest extends TestCase {
             'id' => 'puedo_crear_los_atributos_que_quiera'
         ]));
 
+        //& content type array
         $this->assertEquals('<main class="myClass" id="myId">content</main>',$objetoMain->kMain('myClass','myId',['content']));
 
+        //& content type array
         $this->assertEquals('<main id="myId">content</main>',$objetoMain->jMain('myId',['content']));
 
+        //& content type array
         $this->assertEquals('<main class="myClass">content</main>',$objetoMain->iMain('myClass',['content']));
+
+        //& content type array
+        $this->assertEquals('<main>content</main>',$objetoMain->hMain(['content']));
+
+        //? content type string
+        $this->assertEquals('<main class="myClass" id="myId">content</main>',$objetoMain->gMain('myClass','myId','content'));
+
+        //? content type string
+        $this->assertEquals('<main id="myId">content</main>',$objetoMain->fMain('myId','content'));
+
+        //? content type string
+        $this->assertEquals('<main class="myClass">content</main>',$objetoMain->eMain('myClass','content'));
+
+        //^empty tag
+        $this->assertEquals('<main id="myId"></main>',$objetoMain->dMain('myId'));
+
+        //^empty tag
+        $this->assertEquals('<main class="myClass"></main>',$objetoMain->cMain('myClass'));
+
+        //? content type string
+        $this->assertEquals('<main>content</main>',$objetoMain->bMain('content'));
+
+        //^empty tag
+        $this->assertEquals('<main></main>',$objetoMain->aMain());
 
     }
 
