8f0231a9c58bba52433ba42e73cb2d54ace75a03
rgg7888 <rgg7888@outlook.com>
2021-05-07 12:06:04 -0600

    updating tests

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 echo "<!DOCTYPE html><html><head><title>Piezas4WebSites</title>";
-echo addMaterialize();
+echo addMaterialize()[0];
 echo "</head><body>";
-
+echo addMaterialize()[1];
 echo "</body></html>";
--- tests/Piezas4WebSitesTest.php
+++ tests/Piezas4WebSitesTest.php
@@ -8,7 +8,11 @@ class Piezas4WebSitesTest extends TestCase {
     public function testInstancia(): void
     {
 
-        $this->assertEquals( "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\"/><script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js\"></script>" , Piezas4WebSites::materialize() );
+        $this->assertEquals( "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\"/>" , 
+        Piezas4WebSites::materialize()[0] );
+
+        $this->assertEquals( "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js\" type=\"text/javascript\"></script>" , 
+        Piezas4WebSites::materialize()[1] );
 
     }
 
