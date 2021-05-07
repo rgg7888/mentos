9ac9802e1494a7d7dc85285d830e1708be1dde88
rgg7888 <rgg7888@outlook.com>
2021-05-07 09:30:07 -0600

    adding divTest

--- tests/DivTest.php
+++ tests/DivTest.php
@@ -13,5 +13,38 @@ class DivTest extends TestCase {
         $this->assertInstanceOf(Div::class, $objetoDiv );
 
         $this->assertEquals('OK !',$objetoDiv->ok());
+
+        //& content type array
+        $this->assertEquals('<div class="myClass" id="myId">content</div>',$objetoDiv->kDiv('myClass','myId',['content']));
+
+        //& content type array
+        $this->assertEquals('<div id="myId">content</div>',$objetoDiv->jDiv('myId',['content']));
+
+        //& content type array
+        $this->assertEquals('<div class="myClass">content</div>',$objetoDiv->iDiv('myClass',['content']));
+
+        //& content type array
+        $this->assertEquals('<div>content</div>',$objetoDiv->hDiv(['content']));
+
+        //? content type string
+        $this->assertEquals('<div class="myClass" id="myId">content</div>',$objetoDiv->gDiv('myClass','myId','content'));
+
+        //? content type string
+        $this->assertEquals('<div id="myId">content</div>',$objetoDiv->fDiv('myId','content'));
+
+        //? content type string
+        $this->assertEquals('<div class="myClass">content</div>',$objetoDiv->eDiv('myClass','content'));
+
+        //^empty tag
+        $this->assertEquals('<div id="myId"></div>',$objetoDiv->dDiv('myId'));
+
+        //^empty tag
+        $this->assertEquals('<div class="myClass"></div>',$objetoDiv->cDiv('myClass'));
+
+        //? content type string
+        $this->assertEquals('<div>content</div>',$objetoDiv->bDiv('content'));
+
+        //^empty tag
+        $this->assertEquals('<div></div>',$objetoDiv->aDiv());
     }
 }
