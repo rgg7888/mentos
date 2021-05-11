b957f4eba35166dbc4b00abe3dfc940981706154
rgg7888 <rgg7888@outlook.com>
2021-05-11 12:39:20 -0600

    updating piezas

--- Templates/bodyV2.php
+++ Templates/bodyV2.php
@@ -2,7 +2,11 @@
 use App\Union\Mecanismo\Mecanismo;
 $bodyV2 = body("wrapper grey lighten-4",[
     header_nav ("Pizza Template", "#", "Add a Pizza", "#"),
-    form_template_01("Add a Pizza","Your Email:","Pizza Title:","Ingredients (comma separated):"),
+    form_template_01(
+        "Add a Pizza","Your Email:",
+        "Pizza Title:",
+        "Ingredients (comma separated):"
+    ),
     footer_template_01("center grey-text","Copyright 2021 IG: @ramiroseh"),
     addMaterialize()[1]
 ]);
--- Templates/head.php
+++ Templates/head.php
@@ -9,6 +9,11 @@ $head = head([
         }",
         ".brand-text{
             color: #cbb09c !important;
+        }",
+        "form {
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
         }"
     ],[
         'type' => 'text/css'
