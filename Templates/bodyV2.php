3cb4eff92f475126eca10fb3bd5127cb4f909168
rgg7888 <rgg7888@outlook.com>
2021-05-11 09:23:59 -0600

    updating

--- Templates/bodyV2.php
+++ Templates/bodyV2.php
@@ -2,6 +2,6 @@
 use App\Union\Mecanismo\Mecanismo;
 $bodyV2 = body("wrapper grey lighten-4",[
     header_nav ("Pizza Template", "#", "Add a Pizza", "#"),
-    footer_template_01("center grey-text","Copyright 2021 @ramiroseh"),
+    footer_template_01("center grey-text","Copyright 2021 IG: @ramiroseh"),
     addMaterialize()[1]
 ]);
